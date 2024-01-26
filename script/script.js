e8d94139697f00a05638b1983588d98fd09e157f
Tatyana Kondratyeva <tatyana.k@gmx.net>
2024-01-26 09:05:12 +0100

    init commit

---
+++ script/script.js
@@ -0,0 +1,33 @@
+const formNode = document.querySelector('#addForm');
+const  personalDetails= [];
+
+formNode.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const {lastName, firstName, age} = event.target;
+    const detail = {
+        lastName: lastName.value,
+        firstName: firstName.value,
+        age: age.value
+    }
+    personalDetails.push(detail); 
+    rerender();
+    event.target.reset();
+})
+function getDetailsNode(lastName, firstName, age){
+    const container = document.createElement("div");
+    const lastNameNode = document.createElement("p");
+    const firstNameNode = document.createElement("p");
+    const ageNode = document.createElement("p");
+
+    lastNameNode.innerText = lastName;
+    firstNameNode.innerText = firstName;
+    ageNode.innerText = age;
+    container.append(lastNameNode, firstNameNode, ageNode);
+    return container;
+}
+
+function rerender(){
+    const container =document.querySelector("#cardContainer");
+    container.innerText = '';
+    personalDetails.forEach(({lastName, firstName, age}) => container.append(getDetailsNode(lastName, firstName, age)));
+}
